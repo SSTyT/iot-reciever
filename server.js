09fdcc9b7e25f86eb68169a4593863b6b1d95fff
Agustin Pivetta <aguspivetta@gmail.com>
2016-09-05 17:10:19 -0300

    db

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var PORT = 9876;
 
 var winston = require('winston');
 var dgram = require('dgram');
+var Pool = require('pg').Pool;
 
+//Winston logger
 var logger = new(winston.Logger)({
   transports: [
     new winston.transports.Console(),
@@ -13,12 +15,35 @@ var logger = new(winston.Logger)({
   ]
 });
 
+var onError = function(err) {
+  logger.error(err.message);
+};
+
+process.on('unhandledRejection', onError);
+
+//Postgresql
+var config = {
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'UhbIjn93',
+  database: 'ituran',
+};
+
+var pool = new Pool(config);
+
+//UDP receive
 var server = dgram.createSocket('udp4');
 server.on('listening', function() {
   var address = server.address();
   logger.info('UDP Server listening on ' + address.address + ":" + address.port);
 });
 server.on('message', function(message, remote) {
-  logger.info(message.toString());
+
+  var data = message.toString().split(',');
+  var query = `INSERT INTO reports VALUES ('${data[0]}','${data[1]}','${data[2]}','${data[3]}','${data[4].trim()}-3','${data[5]}','${data[6]}')`;
+  console.log(query);
+  pool.query(query);
 });
+
 server.bind(PORT);
